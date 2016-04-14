df9ebc641e2801e3dff57b2e8b6fbf7afcca092c
i5ting <i5ting@126.com>
2016-04-14 08:29:26 +0800

    add async/await support

--- test/cli.js
+++ test/cli.js
@@ -6,12 +6,7 @@ describe('RUNKOA with cli', function() {
     var coffee = require('coffee');
 
     coffee.spawn('runkoa ' ,[__dirname + '/bin/www'])
-    // .write('1')
-    // .write('2')
-    .expect('stdout', /^abc/)
-    .expect('code', -2)
-    // .end(done);
-    
+
     request('http://127.0.0.1:3000', function (error, response, body) {
       if (!error && response.statusCode == 200) {
         // console.log(body) // Show the HTML for the Google homepage.
@@ -26,12 +21,23 @@ describe('RUNKOA with cli', function() {
   it('should runkoa ' + 'test/bin/www', function(done) {
     var coffee = require('coffee');
 
-    coffee.spawn('runkoa ' ,[__dirname + '/bin/www'])
-    // .write('1')
-    // .write('2')
-    .expect('stdout', /^abc/)
-    .expect('code', -2)
-    // .end(done);
+    coffee.spawn('runkoa ' ,['test/bin/www'])
+
+    request('http://127.0.0.1:3000', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // console.log(body) // Show the HTML for the Google homepage.
+        body.should.equal('Hello Koa in app.js');
+        
+        done()
+      }
+    })
+    
+  })
+  
+  it('should use async/await(ES7) runkoa ' + __dirname + '/bin/async', function(done) {
+    var coffee = require('coffee');
+
+    coffee.spawn('runkoa ' ,[__dirname + '/bin/async'])
     
     request('http://127.0.0.1:3000', function (error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -43,5 +49,22 @@ describe('RUNKOA with cli', function() {
     })
     
   })
+  
+  it('should use async/await(ES7) runkoa ' + 'test/bin/async', function(done) {
+    var coffee = require('coffee');
+
+    coffee.spawn('runkoa ' ,['test/bin/async'])
+    
+    request('http://127.0.0.1:3000', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // console.log(body) // Show the HTML for the Google homepage.
+        body.should.equal('Hello Koa in app.js');
+        
+        done()
+      }
+    })
+    
+  })
+
   
 })
