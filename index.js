3660e7ee9afd9a85d599d2150c749154afa93f0b
Lyman Lai <lyman.lai@yaha.me>
2016-05-07 04:08:45 +0800

    use debug to show log

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
-   
+
 require("babel-polyfill")
 
 var fs = require('fs')
+var debug = require('debug')('runkoa')
 
 var is_npm_v3 = /^3/.test(require('child_process').execSync('npm -v').toString())
 
@@ -14,7 +15,7 @@ var is_npm_v3 = /^3/.test(require('child_process').execSync('npm -v').toString()
   - npm 3是它的上级模块
   - npm 2是当前文件目录下的node_modules\
  *
- */ 
+ */
 module.exports = function (entry, is_cli) {
   if (is_cli === true) {
     get_dirname_in_node_modules()
@@ -25,7 +26,7 @@ module.exports = function (entry, is_cli) {
        get_dirname_in_parent()
     }
   }
-  
+
   var current_path = process.cwd();
   var f = current_path + '/bin/www'
 
@@ -34,45 +35,45 @@ module.exports = function (entry, is_cli) {
   } else {
     f = entry
   }
-  
+
   var is_exist = fs.existsSync(f)
-  
+
   if (is_exist === false) {
     return console.log('runkoa entry file is not exist, please check it');
   }
-  
+
   require(f) // this is es7 - gets transpile
 }
 
 function get_dirname_in_node_modules(){
   var dir =  __dirname + '/node_modules/'
   var plugin_dir = dir + 'babel-plugin-'
-  console.log('3babel presets path = ' + dir)
+  debug('3babel presets path = ' + dir)
   // npm 3.x set babel in entry file
   require('babel-core/register')({
     plugins: [plugin_dir + 'add-module-exports', plugin_dir + 'transform-es2015-modules-commonjs'],
     presets: [dir + 'babel-preset-es2015-node5', dir + 'babel-preset-stage-3'],
     babelrc: false
-  })  
+  })
 }
 
 function get_dirname_in_parent(){
   var dir =  __dirname.replace('runkoa', '')
 
   var plugin_dir = dir + 'babel-plugin-'
-  
-  
+
+
   if(is_npm_v3 === true && process.env.RUNKOA_TEST){
     dir = dir + 'runkoa/node_modules/'
     plugin_dir = dir + 'babel-plugin-'
   }
-  
-  console.log('2babel presets path = ' + dir)
-  
+
+  debug('2babel presets path = ' + dir)
+
   // npm 2.x set babel in entry file
   require('babel-core/register')({
     plugins: [plugin_dir + 'add-module-exports', plugin_dir + 'transform-es2015-modules-commonjs'],
     presets: [dir + 'babel-preset-es2015-node5', dir + 'babel-preset-stage-3'],
     babelrc: false
-  }) 
+  })
 }
